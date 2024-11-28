ba7dc64d4e5788b8617ae0e4848d5a9319d17748
Hung Le <hung.le2@tyme.com>
2024-11-28 13:14:42 +0700

    add secrets to the app

--- user-service/app/main.py
+++ user-service/app/main.py
@@ -1,22 +1,29 @@
 from fastapi import FastAPI
+import logging
+import os
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 from api import login, admin, vendor, customer
 from config.db.gino_db import db
 
-app = FastAPI()
-
-
+app = FastAPI(docs_url="/user", redoc_url=None)
+POSTGRES_PASSWORD=os.getenv("POSTGRES_PASSWORD", "default_password")
+logger.info(f"{POSTGRES_PASSWORD}")
 @app.on_event("startup")
 async def initialize():
-    engine = await db.set_bind(
-        "postgresql+asyncpg://postgres:123@host.docker.internal:5432/public"
-    )
-
+    try:
+        await db.set_bind(f"postgresql+asyncpg://postgres:{POSTGRES_PASSWORD}@postgresql:5432/public")
+        logger.info("Database connected successfully.")
+    except Exception as e:
+        logger.error(f"Error during startup: {e}")
+        raise
 
 @app.on_event("shutdown")
 async def destroy():
-    engine, db.bind = db.bind, None
-    await engine.close()
+    if db.bind is not None:
+        await db.pop_bind().close()
+        logger.info("Database connection closed.")
 
 
 app.include_router(login.router, prefix="/ch08")
